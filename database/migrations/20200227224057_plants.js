dc90ce088ed2bd328bcb00aeda1a01af4d66be94
Pamela Sage <pamelasage3@gmail.com>
2020-03-06 10:09:22 -0700

    update plants data

--- database/migrations/20200227224057_plants.js
+++ database/migrations/20200227224057_plants.js
@@ -2,10 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("plants", tbl => {
         tbl.increments();
-        tbl.string("name", 255).notNullable();
+        tbl.string("nickName", 255).notNullable();
         tbl.string("species", 255);
         tbl.string("h2oFrequency");
-        tbl.string("nickname", 255);
         tbl.string("image");
         tbl.string("dateLastWatered");
         tbl.string("notificationTime");
--- database/seeds/002-plants.js
+++ database/seeds/002-plants.js
@@ -6,35 +6,32 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('plants').insert([
         {
-          name: 'fern',
+          nickName: 'Fernie',
           species: 'Birds-nest fern',
-          h2oFrequency: 'weekly',
-          nickname: 'Fernie',
+          h2oFrequency: '7',
           image: "https://www.ourhouseplants.com/imgs-content/large_bird_nest_fern.jpg",
-          dateLastWatered: "yesterday",
-          notificationTime: "9 am",
+          dateLastWatered: "480",
+          notificationTime: "08:30:00",
           notificationEnabled: false
         },
 
         {
-          name: 'bamboo',
+          nickName: 'Panda food',
           species: 'Dracaena braunii',
-          h2oFrequency: 'twice a week',
-          nickname: 'Panda food',
+          h2oFrequency: '6',
           image: "https://res.cloudinary.com/bloomnation/c_pad,d_vendor:global:catalog:product:image.png,f_auto,fl_preserve_transparency,q_auto/v1546919669/vendor/4366/catalog/product/2/0/20161117031919_file_582d21b7890a3_2.jpg",
-          dateLastWatered: "Monday",
-          notificationTime: "12 pm",
+          dateLastWatered: "470",
+          notificationTime: "09:00:00",
           notificationEnabled: false
         },
 
         {
-          name: 'Spider Ivy',
+          nickName: 'Spidey',
           species: 'perennial flowering',
-          h2oFrequency: 'daily',
-          nickname: 'Spidey',
+          h2oFrequency: '5',
           image: "https://5.imimg.com/data5/ZP/WL/CY/SELLER-91354607/spider-plant-jpg-500x500.jpg",
-          dateLastWatered: "Saturday",
-          notificationTime: "4 pm",
+          dateLastWatered: "460",
+          notificationTime: "10:00:00",
           notificationEnabled: "false"
         }
       ]);
